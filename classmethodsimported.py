537400d1f665ead933c9f482faa50a5ac45ebb17
karan singh <karan.singh@calsoftinc.com>
2021-02-17 16:56:10 +0530

    decorator added to classmethodsimported.py

--- classmethodsimported.py
+++ classmethodsimported.py
@@ -1,9 +1,20 @@
 from FunctionsFile import ist
+import time
+def sleepdec(func):
+    def inner(x):
+        for i in range(10):
+            time.sleep(1)
+            print(str(i+1)+"\n")
+        return func(x)
+    return inner
+        
 a=ist()
 b=[1,2,3]
 c={"one":1,
     "two":2
    }
-a.addElementToList(b)
-a.addElementToDict(c)
+addElementToList1=sleepdec(a.addElementToList)
+addElementToList1(b)
+addElementToDict1=sleepdec(a.addElementToDict)
+addElementToDict1(c)
 a.printObject()
